2ba55f29a7919af3ed42094e5022311357db1298
= <=>
2017-07-19 23:35:14 +0530

    commiting new chapters

---
+++ buffers.js
@@ -0,0 +1,11 @@
+var buf = new Buffer(26);
+
+console.log(buf);
+
+var buf1 = new Buffer([10,20,30,40,50]);
+
+console.log(buf1);
+
+var buf2 = new Buffer('This is from tajinder singh minhas');
+
+console.log(buf2.toString());
